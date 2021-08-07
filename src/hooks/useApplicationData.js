f2de991fbd4bab5627f2637f9dbac61eb6b7e434
Veronica Leung <tungtungleung233@hotmail.com>
2021-08-07 15:09:06 -0400

    implement updateSpots function - spots remaining auto updates after saving/deleting an appointment

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -30,6 +30,43 @@ export default function useApplicationData() {
   }, [])
 
 
+  function updateSpots(state) {
+    const currentDay = state.day;
+
+    //contains all the details about that day - including interviews scheduled
+    const currentDayObj = state.days.find(dayObj => dayObj.name === currentDay)
+    const currentDayObjIndex = state.days.findIndex(dayObj => dayObj.name === currentDay)
+
+    //containts an array of the current day's appointment IDs
+    const currentDayAllApps = currentDayObj.appointments;
+
+    const currentDayFreeApps = currentDayAllApps.filter(appId => !state.appointments[appId].interview)
+
+    const freeSpots = currentDayFreeApps.length;
+
+    //we need to update the sate with the new number of freeSpots - 
+    //in state, spots is found under days --> days[{name: "Monday", spots: 4}, {name: "Tuesday", spots: 2}]
+    // ^notice how this is an *array* of objects, hence why we need to find the index of the current day in the days array
+    //cannot directly change the state - right now to get the value of 'freeSpots' we are directly referencing the state (but not manipulating it)
+
+    const updatedState = { ... state }
+
+    //makes a copy of the days array from state, rather than referencing it
+    updatedState.days = [ ... state.days]
+    //makes a copy of the current day object from state ('currentDayObj' was a reference to the state, so updating spots in currentDayObj would change the state)
+    const updatedDay = { ... currentDayObj}
+    //updates the number of free spots in the copy of the current day obj
+    updatedDay.spots = freeSpots
+    //updates the entire current day object in the copied state object
+    updatedState.days[currentDayObjIndex] = updatedDay
+
+
+    return updatedState;
+  }
+
+
+  // updateSpots(state)
+
   //id = appointment id
   //interview = details about student and interviewer
   function bookInterview(id, interview) {
@@ -44,12 +81,14 @@ export default function useApplicationData() {
           ...state.appointments,
           [id]: appointment
         };
+
         
         setState({
           ...state,
           appointments
         })
 
+        setState(prev => updateSpots(prev))
       })
   }
 
@@ -70,6 +109,8 @@ export default function useApplicationData() {
           ...state,
           appointments
         });
+
+        setState(prev => updateSpots(prev))
       })
   }
 
