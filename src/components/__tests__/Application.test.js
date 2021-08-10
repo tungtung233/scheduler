0f605644e3b0e0e2c29f68f6c6c806816213fe68
Veronica Leung <tungtungleung233@hotmail.com>
2021-08-10 16:36:59 -0400

    add tests for deleting/saving errors

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import { 
   render, 
@@ -45,7 +46,7 @@ describe("Application", () => {
       target: { value: "Lydia Miller-Jones" }
     });
 
-    //Sylvia Palmer's appointment is the one we are editing
+    //Sylvia Palmer is the interviewer we are selecting
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     
     fireEvent.click(getByText(appointment, "Save"));
@@ -135,4 +136,78 @@ describe("Application", () => {
     expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
 
   });
+
+
+  it("shows the save error when failing to save an appointment", async () => {
+    axios.put.mockRejectedValueOnce();
+
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+
+    //gets all the appointments for the day (articles), in an array
+    const appointments = getAllByTestId(container, "appointment");
+    // reference the first appointment - it is a blank appointment
+    const appointment = appointments[0]
+    
+    //an empty appointment will show the plus icon, alt-text is 'Add'
+    fireEvent.click(getByAltText(appointment, "Add"));
+    
+    //enter the value 'Lydia Miller-Jones' as the student name
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    //Sylvia Palmer's appointment is the one we are editing
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    
+    fireEvent.click(getByText(appointment, "Save"));
+
+    //Verifing that after entering a new appointment, we see the 'Saving' component
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    //'Could not save appointment' is the error message that should appear
+    await waitForElement(() => getByText(appointment, "Could not save appointment"));
+
+    //Closing the error message 
+    fireEvent.click(getByAltText(appointment, "Close"))
+
+    //an empty appointment will show the plus icon, alt-text is 'Add'
+    expect(getByAltText(appointment, "Add")).toBeInTheDocument();
+
+  });
+  
+
+  it("shows the delete error when failing to delete an existing appointment", async () => {
+    axios.delete.mockRejectedValueOnce();
+
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"))
+
+    //gets all the appointments for the day (articles), in an array, then finds the appointment booked by 'Archie Cohen'
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    
+    //a booked appointment will show the delete icon, alt-text is 'Delete'
+    fireEvent.click(getByAltText(appointment, "Delete"));
+
+    expect(getByText(appointment, "Delete the appointment?")).toBeInTheDocument();
+
+    fireEvent.click(getByText(appointment, "Confirm"));
+
+    //Verifing that after entering a new appointment, we see the 'Deleting' component
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+
+    //'Could not delete appointment' is the error message that should appear
+    await waitForElement(() => getByText(appointment, "Could not delete appointment"));
+
+    //Closing the error message 
+    fireEvent.click(getByAltText(appointment, "Close"))
+
+    //Verify that Archie Cohen's appointment is still there
+    expect(queryByText(appointment, "Archie Cohen")).toBeInTheDocument();
+
+  });
+
+  
 })
